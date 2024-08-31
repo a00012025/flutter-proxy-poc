5160b7e38fc4677c0be9ae92206c3800d0dcd6da
Harry Chen <a00012025@gmail.com>
2024-08-31 22:49:31 +0800

    feat: proxy test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +12,111 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: ProxyDemo(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ProxyDemo extends StatefulWidget {
+  const ProxyDemo({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ProxyDemoState createState() => _ProxyDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ProxyDemoState extends State<ProxyDemo> {
+  HttpServer? _server;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    startProxy();
+  }
+
+  @override
+  void dispose() {
+    _server?.close();
+    super.dispose();
+  }
+
+  Future<void> startProxy() async {
+    try {
+      // Load the key from the assets and write it to a temporary file
+      final keyData = await rootBundle.load('assets/harry-proxy.key');
+      final tempDir = await getApplicationDocumentsDirectory();
+      final keyFile = File('${tempDir.path}/harry-proxy.key');
+      await keyFile.writeAsBytes(keyData.buffer.asUint8List());
+
+      // Load the certificate from the assets and write it to a temporary file
+      final certData = await rootBundle.load('assets/harry-proxy.crt');
+      final certFile = File('${tempDir.path}/harry-proxy.crt');
+      await certFile.writeAsBytes(certData.buffer.asUint8List());
+
+      // Loading the certificate and key
+      final securityContext = SecurityContext()
+        ..useCertificateChain(certFile.path)
+        ..usePrivateKey(keyFile.path);
+
+      // Bind the server
+      _server = await HttpServer.bindSecure(
+        InternetAddress.anyIPv4,
+        8080,
+        securityContext,
+      );
+
+      print('Proxy server running on https://localhost:8080');
+
+      await for (HttpRequest request in _server!) {
+        // Extract and log the requested URL
+        final uri = request.uri.toString();
+        print('Intercepted URL: $uri');
+
+        // Forward the request to the intended server
+        final HttpClient httpClient = HttpClient(context: securityContext);
+        final HttpClientRequest clientRequest =
+            await httpClient.openUrl(request.method, request.uri);
+
+        // Copy headers
+        request.headers.forEach((name, values) {
+          for (var value in values) {
+            clientRequest.headers.add(name, value);
+          }
+        });
+
+        // Copy body if it exists
+        if (request.contentLength > 0) {
+          await request.pipe(clientRequest as StreamConsumer<Uint8List>);
+        }
+
+        // Get response from the server
+        final HttpClientResponse clientResponse = await clientRequest.close();
+
+        // Send the response back to the client
+        request.response.statusCode = clientResponse.statusCode;
+        clientResponse.headers.forEach((name, values) {
+          for (var value in values) {
+            request.response.headers.add(name, value);
+          }
+        });
+        await clientResponse.pipe(request.response);
+      }
+    } catch (e) {
+      print('Failed to start proxy: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('HTTPS Proxy Demo'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text('Proxy is running. Check console for intercepted URLs.'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
