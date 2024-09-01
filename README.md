909f84017ebefae0ab8730a1118071e4c52c75a9
Harry Chen <a00012025@gmail.com>
2024-09-01 15:03:59 +0800

    feat: make proxy work

--- README.md
+++ README.md
@@ -14,3 +14,14 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Setup
+
+```bash
+openssl genrsa -out harry-local.key 2048
+openssl req -x509 -new -nodes -key harry-local.key -sha256 -days 3650 -out harry-local.pem
+openssl genrsa -out harry-proxy.key 2048
+openssl req -new -key harry-proxy.key -out harry-proxy.csr
+openssl x509 -req -in harry-proxy.csr -CA harry-local.pem -CAkey harry-local.key -CAcreateserial -out harry-proxy.crt -days 365 -sha256
+openssl pkcs12 -export -out harry-proxy.p12 -inkey harry-proxy.key -in harry-proxy.crt -certfile harry-local.pem
+```
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +31,7 @@ class _ProxyDemoState extends State<ProxyDemo> {
   @override
   void initState() {
     super.initState();
-    startProxy();
+    startTcpProxy();
   }
 
   @override
@@ -42,128 +40,88 @@ class _ProxyDemoState extends State<ProxyDemo> {
     super.dispose();
   }
 
-  Future<void> startProxy() async {
-    try {
-      final certDir = await getApplicationDocumentsDirectory();
+  Future<void> startTcpProxy() async {
+    final server = await ServerSocket.bind(InternetAddress.anyIPv4, 8080);
+    print('TCP Proxy Server is running on port 8080');
 
-      // Load the certificate and key from assets
-      final certData = await rootBundle.load('assets/harry-proxy.crt');
-      final keyData = await rootBundle.load('assets/harry-proxy.key');
+    await for (Socket clientSocket in server) {
+      clientSocket.listen((Uint8List data) async {
+        String request = String.fromCharCodes(data);
+        if (request.startsWith('CONNECT')) {
+          // Parse the target host and port from the CONNECT request
+          final targetInfo = request.split(' ')[1];
+          final targetHost = targetInfo.split(':')[0];
+          final targetPort = int.parse(targetInfo.split(':')[1]);
 
-      final certPath = '${certDir.path}/harry-proxy.crt';
-      final keyPath = '${certDir.path}/harry-proxy.key';
+          // Respond with "200 Connection Established"
+          clientSocket.write('HTTP/1.1 200 Connection Established\r\n\r\n');
 
-      // Write cert and key to file
-      final certFile = File(certPath);
-      final keyFile = File(keyPath);
-      await certFile.writeAsBytes(certData.buffer.asUint8List());
-      await keyFile.writeAsBytes(keyData.buffer.asUint8List());
+          // Upgrade the socket to a SecureSocket (Server-side TLS)
+          final p12Data = await rootBundle.load('assets/harry-proxy.p12');
+          final p12Bytes = p12Data.buffer.asUint8List();
+          SecurityContext context = SecurityContext()
+            ..useCertificateChainBytes(p12Bytes, password: 'abc')
+            ..usePrivateKeyBytes(p12Bytes, password: 'abc')
+            ..setAlpnProtocols(['http/1.1'], true);
 
-      // Setup SecurityContext
-      final securityContext = SecurityContext()
-        ..useCertificateChain(certPath)
-        ..usePrivateKey(keyPath);
+          SecureSocket secureClientSocket = await SecureSocket.secureServer(
+            clientSocket,
+            context,
+            supportedProtocols: ['http/1.1'],
+          );
 
-      // Start the proxy server
-      _server = await HttpServer.bind(InternetAddress.anyIPv4, 8080);
-
-      print(
-          'Proxy server running on http://${_server!.address.address}:${_server!.port}');
-
-      await for (HttpRequest request in _server!) {
-        handleRequest(request, securityContext);
-      }
-    } catch (e) {
-      print('Failed to start proxy: $e');
+          // Handle the CONNECT method by establishing a connection to the target server
+          await _handleTlsMitm(secureClientSocket, targetHost, targetPort);
+        }
+      });
     }
   }
 
-  void handleRequest(
-      HttpRequest request, SecurityContext securityContext) async {
+  Future<void> _handleTlsMitm(
+      SecureSocket clientSocket, String targetHost, int targetPort) async {
     try {
-      final uri = request.uri;
-      print('Intercepted URL: $uri');
+      // Establish a TLS connection to the target server
+      final SecureSocket targetSocket = await SecureSocket.connect(
+        targetHost,
+        targetPort,
+        context: SecurityContext.defaultContext,
+        supportedProtocols: ['http/1.1'],
+      );
 
-      // Create a new HttpClient to forward the request
-      final httpClient = HttpClient(context: securityContext);
-      httpClient.badCertificateCallback = (cert, host, port) => true;
+      // Forward data from the client to the target server
+      clientSocket.listen(
+        (data) {
+          targetSocket.add(data);
+        },
+        onError: (error) {
+          print('Error in client socket: $error');
+          targetSocket.destroy();
+        },
+        onDone: () {
+          print('Client socket closed.');
+          targetSocket.destroy();
+        },
+        cancelOnError: true,
+      );
 
-      final isConnect = request.method == 'CONNECT';
-      final targetUri =
-          isConnect ? Uri.parse('https://${uri.toString()}') : uri;
-      print('Target URI: $targetUri'); // Add this line for debugging
-
-      if (isConnect) {
-        // Handle CONNECT method for HTTPS
-        final socket = await request.response.detachSocket();
-
-        // Check if the host is not empty before attempting to connect
-        if (targetUri.host.isNotEmpty) {
-          final secureSocket = await SecureSocket.connect(
-            targetUri.host,
-            targetUri.port,
-            context: securityContext,
-            onBadCertificate: (cert) => true,
-          );
-
-          await socket.addStream(secureSocket);
-          await secureSocket.addStream(socket);
-
-          final clientRequest = await httpClient.getUrl(targetUri);
-          final clientResponse = await clientRequest.close();
-
-          await secureSocket.addStream(clientResponse);
-          await socket.addStream(secureSocket);
-        } else {
-          print('Error: Empty host in CONNECT request');
-          socket.destroy();
-        }
-      } else {
-        // Handle regular HTTP requests
-        final clientRequest =
-            await httpClient.openUrl(request.method, targetUri);
-
-        // Copy the headers, excluding Content-Length
-        request.headers.forEach((name, values) {
-          if (name.toLowerCase() != 'content-length') {
-            for (var value in values) {
-              clientRequest.headers.add(name, value);
-            }
-          } else {
-            print('Skipping Content-Length header. Value: ${values.first}');
-          }
-        });
-
-        // Copy the body and calculate new content length
-        List<int> bodyBytes = [];
-        await for (var chunk in request) {
-          bodyBytes.addAll(chunk);
-        }
-
-        if (bodyBytes.isNotEmpty) {
-          clientRequest.contentLength = bodyBytes.length;
-          clientRequest.add(bodyBytes);
-        }
-
-        await clientRequest.close();
-
-        final clientResponse = await clientRequest.close();
-
-        // Set the response headers
-        request.response.statusCode = clientResponse.statusCode;
-        clientResponse.headers.forEach((name, values) {
-          for (var value in values) {
-            request.response.headers.add(name, value);
-          }
-        });
-
-        // Pipe the response back to the client
-        await request.response.addStream(clientResponse);
-      }
-    } catch (e, s) {
-      print('Error handling request: $e , $s');
-      // request.response.statusCode = HttpStatus.internalServerError;
-      await request.response.close();
+      // Forward data from the target server to the client
+      targetSocket.listen(
+        (data) {
+          clientSocket.add(data);
+        },
+        onError: (error) {
+          print('Error in target socket: $error');
+          clientSocket.destroy();
+        },
+        onDone: () {
+          print('Target socket closed.');
+          clientSocket.destroy();
+        },
+        cancelOnError: true,
+      );
+    } catch (e) {
+      print('Error during MITM TLS handling: $e');
+      clientSocket.destroy();
     }
   }
 
@@ -171,9 +129,9 @@ class _ProxyDemoState extends State<ProxyDemo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HTTPS Proxy Demo'),
+        title: const Text('HTTPS Proxy Demo'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Proxy is running. Check console for intercepted URLs.'),
       ),
     );
